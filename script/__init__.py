36d3eb98153209f584942c59b343c6437e80f927
DHI\wuw <wuw@dhigroup.com>
2023-08-30 16:00:44 +0800

    Code clear

--- script/__init__.py
+++ script/__init__.py
@@ -1,14 +1,10 @@
 import clr
-import sys
-import os
-from platform import architecture
 
-__version__ = "0.3.0"
+__version__ = "0.1.0"
 
 clr.AddReference("DHI.Mike.Install, Version=1.0.0.0, Culture=neutral, PublicKeyToken=c513450b5d0bf0bf")
 from DHI.Mike.Install import MikeImport, MikeProducts
 MikeImport.Setup(22, MikeProducts.MikePlus)
-'''MikeImport.SetupInstallDir("D:\\Work\\10081016 - MIKE+ WD features 2024\\Products\\Source\\Amelia\\Core\\Source", True)'''
 
 clr.AddReference("System")
 clr.AddReference("System.Runtime")
