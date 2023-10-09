426248a81981c1c31cdd91576a00dcc6c671a7f3
wuwwen <wuw@dhigroup.com>
2023-10-09 14:40:34 +0800

    Update import_tool.py
    Adapt to PEP8

--- script/tools/import_tool.py
+++ script/tools/import_tool.py
@@ -4,18 +4,18 @@ from DHI.Amelia.Tools.ImportTool.ImportEngine import ImportToolBase
 
 
 class ImportTool:
-	def __init__(self,
-				 configFile,
-				 dataTables=None):
-		self._configFile = os.path.abspath(configFile)
-		self._dataTables = dataTables
-	
-	def run(self):
-		import_engine = ImportToolBase(self._dataTables)
-		import_engine.Load(self._configFile)
-		FunctionHelper.ChangeFilePathInConfigToAbsolute(import_engine.JobConfigSectionsDic, self._configFile)
-		import_engine.OnTableDataProcessing += self._on_table_data_processing
-		import_engine.Run()
-		
-	def _on_table_data_processing(self, source, args):
-		print(args.TileInfo)
+    def __init__(self,
+                 configFile,
+                 dataTables=None):
+        self._configFile = os.path.abspath(configFile)
+        self._dataTables = dataTables
+
+    def run(self):
+        import_engine = ImportToolBase(self._dataTables)
+        import_engine.Load(self._configFile)
+        FunctionHelper.ChangeFilePathInConfigToAbsolute(import_engine.JobConfigSectionsDic, self._configFile)
+        import_engine.OnTableDataProcessing += self._on_table_data_processing
+        import_engine.Run()
+
+    def _on_table_data_processing(self, source, args):
+        print(args.TileInfo)
