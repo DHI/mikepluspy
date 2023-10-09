f777c56ee4755d8450ccd98eabc27bee051b8b28
wuwwen <wuw@dhigroup.com>
2023-10-09 14:40:01 +0800

    Update connection_repair_tool.py
    Adapt to PEP8

--- script/tools/connection_repair_tool.py
+++ script/tools/connection_repair_tool.py
@@ -1,14 +1,15 @@
 from DHI.Amelia.Tools.ConnectionRepairEngine import ConnectionRepairEngine
 
+
 class ConnectionRepairTool:
-	def __init__(self,
-				 dataTables):
-		self._dataTables = dataTables
-	
-	def run(self):
-		tool = ConnectionRepairEngine(self._dataTables)
-		tool.Run()
-		tool.RuningProgress += self._on_tool_runing_progress
-		
-	def _on_tool_runing_progress(self, source, args):
-		print(args.Msg)
+    def __init__(self,
+                 dataTables):
+        self._dataTables = dataTables
+
+    def run(self):
+        tool = ConnectionRepairEngine(self._dataTables)
+        tool.Run()
+        tool.RuningProgress += self._on_tool_runing_progress
+
+    def _on_tool_runing_progress(self, source, args):
+        print(args.Msg)
