72e228ebf6bfbef709982e699c6214a696544612
Ryan Walter Murray <rywm@dhigroup.com>
2024-11-01 09:23:21 +0100

    Print shell config xml for debugging in CI actions

--- install.ps1
+++ install.ps1
@@ -57,3 +57,6 @@ if ($fileContent -match $regexPattern) {
 
 Copy-Item -Path $tempFile -Destination $SHELL_CONFIG_FILE -Force
 Remove-Item -Path $tempFile -Force
+
+Write-Host "Contents of $SHELL_CONFIG_FILE:"
+Get-Content -Path $SHELL_CONFIG_FILE | ForEach-Object { Write-Host $_ }
