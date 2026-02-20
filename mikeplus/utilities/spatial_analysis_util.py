9d28338b8f1ab601830c600298d066bc4ef22008
wuwwen <wuw@dhigroup.com>
2026-02-20 15:56:24 +0100

    Fix a river name bug and add more spatial anaylis method.

--- mikeplus/utilities/spatial_analysis_util.py
+++ mikeplus/utilities/spatial_analysis_util.py
@@ -29,19 +29,46 @@ class SpatialAnalysisUtil:
             first value is the river name, second value is the chainage value
 
         """
-        river = database.tables.mrm_Branch._net_table.GetNearestMuid(
+        river_id = database.tables.mrm_Branch._net_table.GetNearestMuid(
             x, y, tolorance, None, None
         )
-        if river is not None:
-            riverGeom = database.tables.mrm_Branch._net_table.GetGeometry(river)
+        if river_id is not None:
+            riverGeom = database.tables.mrm_Branch._net_table.GetGeometry(river_id)
             lineGeom = GeoAPIHelper.GetWKBIGeometry(riverGeom)
             lineShape = BaseShape.CreateShapeFromWellKnownData(lineGeom)
             pointshp = PointShape(x, y)
             locationPnt = lineShape.GetClosestPointTo(pointshp, GeographyUnit.Meter)
+            river_name = database.tables.mrm_Branch._net_table.GetName(river_id)
             chainageMngr = database._data_table_container.GetChainageManager(
-                "mrm_Branch", river
+                "mrm_Branch", river_name
             )
             chainage = chainageMngr.GetChainageAt(locationPnt, tolorance)
-            return [river, chainage]
+            return [river_name, chainage]
         else:
             return None
+
+    @staticmethod
+    def get_nearest_river_at(database, x: float, y: float, tolorance: float):
+        """Get the nearest river name by a give (x,y) location.
+
+        Parameters
+        ----------
+        database : Database or DataTables
+        x : float
+            x coordinate of target search location
+        y : float
+            y coordinate of target search location
+        tolorance : float
+            search radiu distance
+
+        Returns
+        -------
+        str:
+            river name
+
+        """
+        river_id = database.tables.mrm_Branch._net_table.GetNearestMuid(
+            x, y, tolorance, None, None
+        )
+        river_name = database.tables.mrm_Branch._net_table.GetName(river_id)
+        return river_name
