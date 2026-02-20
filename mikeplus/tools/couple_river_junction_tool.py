3da4d94f801032edbec7a5f423a55a71f156a868
wuwwen <wuw@dhigroup.com>
2026-02-20 10:52:47 +0100

    Fix the performance issue.

--- mikeplus/tools/couple_river_junction_tool.py
+++ mikeplus/tools/couple_river_junction_tool.py
@@ -50,25 +50,35 @@ class CoupleRiverJunctionTool:
             .where("TypeNo=6 AND BranchID is NULL")
             .execute()
         )
-        for key, value in df.items():
-            x = value[0]
-            y = value[1]
-            river = self.db.tables.mrm_Branch._net_table.GetNearestMuid(
-                x, y, 100.0, None, None
-            )
-            if river is not None:
-                riverGeom = self.db.tables.mrm_Branch._net_table.GetGeometry(river)
-                lineGeom = GeoAPIHelper.GetWKBIGeometry(riverGeom)
-                lineShape = BaseShape.CreateShapeFromWellKnownData(lineGeom)
-                pointshp = PointShape(x, y)
-                locationPnt = lineShape.GetClosestPointTo(pointshp, GeographyUnit.Meter)
-                chainageMngr = self.db._data_table_container.GetChainageManager(
-                    "mrm_Branch", river
+        self.db._data_table_container.BeginTransaction()
+        commit = True
+        try:
+            for key, value in df.items():
+                x = value[0]
+                y = value[1]
+                river = self.db.tables.mrm_Branch._net_table.GetNearestMuid(
+                    x, y, 100.0, None, None
                 )
-                chainage = chainageMngr.GetChainageAt(locationPnt, 100.0)
-                self.db._tables.msm_Node.update(
-                    {"BranchID": river, "BranchChainage": chainage}
-                ).by_muid(key).execute()
-                print(
-                    f"River junction of '{key}' has been coupled to '{river}' at {chainage}"
-                )
+                if river is not None:
+                    riverGeom = self.db.tables.mrm_Branch._net_table.GetGeometry(river)
+                    lineGeom = GeoAPIHelper.GetWKBIGeometry(riverGeom)
+                    lineShape = BaseShape.CreateShapeFromWellKnownData(lineGeom)
+                    pointshp = PointShape(x, y)
+                    locationPnt = lineShape.GetClosestPointTo(
+                        pointshp, GeographyUnit.Meter
+                    )
+                    chainageMngr = self.db._data_table_container.GetChainageManager(
+                        "mrm_Branch", river
+                    )
+                    chainage = chainageMngr.GetChainageAt(locationPnt, 100.0)
+                    self.db._tables.msm_Node.update(
+                        {"BranchID": river, "BranchChainage": chainage}
+                    ).by_muid(key).execute()
+                    print(
+                        f"River junction of '{key}' has been coupled to '{river}' at {chainage}"
+                    )
+        except RuntimeError as e:
+            print(f"An error occurred: {e}")
+            commit = False
+
+        self.db._data_table_container.EndTransaction(commit)
